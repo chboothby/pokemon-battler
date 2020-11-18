7ec6879bbe6c18ff375a16278646e40d5f8fd356
Clara <clarahboothby@gmail.com>
2020-11-18 07:43:04 +0000

    setup Pokemon contstructor class

---
+++ pokemon.js
@@ -0,0 +1,38 @@
+class Pokemon {
+  constructor(name, hitPoints, attackDamage, sound, move, type) {
+    this.name = name;
+    this.hitPoints = hitPoints;
+    this.attackDamage = attackDamage;
+    this.sound = sound;
+    this.move = move;
+    this.strength = [];
+    this.weakness = [];
+  }
+
+  talk() {
+    return this.sound;
+  }
+
+  useYourMoves() {
+    return this.moves;
+  }
+
+  setStrengthAndWeakness() {
+    switch (this.type) {
+      case "fire":
+        this.strength.push("grass");
+        this.weakness.push("water");
+        break;
+      case "grass":
+        this.strength.push("water");
+        this.weakness.push("fire");
+        break;
+      case "water":
+        this.strength.push("fire");
+        this.weakness.push("grass");
+        break;
+    }
+  }
+}
+
+module.exports = { Pokemon };
