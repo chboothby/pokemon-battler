d6b7433ef2edad8005386ff4cc3a79e3764847a4
Clara <clarahboothby@gmail.com>
2020-11-18 14:06:11 +0000

    added inquirer funcitonality enabling pokemon battler to be played in the command line

--- play-game.js
+++ play-game.js
@@ -1,8 +1,87 @@
 const inquirer = require("inquirer");
 const { Battle } = require("./battle");
 const { Trainer } = require("./trainer");
-const { Pokemon, pokeDeck } = require("./battle");
+const {
+  Pokemon,
+  rattata,
+  jigglypuff,
+  squirtle,
+  bulbasaur,
+} = require("./pokemon");
 
-console.log(`Hi there! Are you ready to play some Pokemon?!`);
+console.log(`Are you ready to play some Pokemon?!`);
 
-const questions = [{ type: "input", name: "pressEnter" }];
+const questions = [
+  { type: "input", name: "name", message: "Hi there! What is your name?" },
+  {
+    type: "list",
+    name: "pokemon",
+    message: "Time to catch some pokemon!",
+    choices: ["Bulbasaur", "Squirtle", "Rattata", "Jigglypuff"],
+  },
+  {
+    type: "list",
+    name: "opponent",
+    message: "Choose your opponent",
+    choices: ["Giovanni"],
+  },
+];
+
+const nextQuestions = [
+  {
+    type: "list",
+    name: "turn",
+    message: "What is your next move?",
+    choices: ["Fight", "Run"],
+  },
+];
+
+const takeTurn = () => {
+  inquirer.prompt(nextQuestions).then((answers) => {
+    if (answers.turn === "Fight") {
+      if (thisBattle.winner === false) {
+        thisBattle.fight();
+        if (thisBattle.winner === false) {
+          thisBattle.fight();
+          if (thisBattle.winner === false) {
+            takeTurn();
+          }
+        }
+      }
+    } else if (answers.turn === "Run") {
+      console.log("Too bad, come back when you are ready!");
+    }
+  });
+};
+
+inquirer.prompt(questions).then((answers) => {
+  const player = new Trainer(answers.name);
+  const opponent = new Trainer(answers.opponent);
+
+  if (answers.pokemon === "Bulbasaur") {
+    player.catch(bulbasaur);
+    opponent.catch(squirtle);
+  }
+  if (answers.pokemon === "Jigglypuff") {
+    player.catch(jigglypuff);
+    opponent.catch(rattata);
+  }
+  if (answers.pokemon === "Squirtle") {
+    player.catch(squirtle);
+    opponent.catch(bulbasaur);
+  }
+  if (answers.pokemon === "Rattata") {
+    player.catch(rattata);
+    opponent.catch(jigglypuff);
+  }
+
+  thisBattle = new Battle(
+    player,
+    opponent,
+    Object.values(player.storage)[0],
+    Object.values(opponent.storage)[0]
+  );
+
+  console.log(`Let the battle commence!`);
+  takeTurn();
+});
